bc6752da91c8f014d7cf482cf1a1786c78f8a12d
GiorgioDordo <djole6789@gmail.com>
2025-04-23 16:28:23 +0200

    Add CORS policy and configure database contexts
    - Introduced necessary using directives for EF Core and JSON serialization.
    - Updated `AddControllers` to ignore reference cycles in JSON.
    - Added a CORS policy allowing all origins, headers, and methods.
    - Registered `AdventureWorksDataContext` and `AdventureWorksLt2019Context` with SQL Server configurations.
    - Enabled the CORS policy in the application pipeline.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using RideWild.DataModels;
+using RideWild.Models;
+using System.Text.Json.Serialization;
+
 namespace RideWild
 {
     public class Program
@@ -13,8 +18,31 @@ namespace RideWild
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            builder.Services.AddControllers().AddJsonOptions(jsOpt =>
+                jsOpt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CORSPolicy", policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .SetIsOriginAllowed(origin => true);
+                });
+            });
+
+            builder.Services.AddDbContext<AdventureWorksDataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksData")));
+
+            builder.Services.AddDbContext<AdventureWorksLt2019Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AdventureWorksLT2019")));
+
+
+
+            var app = builder.Build();
+            app.UseCors("CORSPolicy");
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
