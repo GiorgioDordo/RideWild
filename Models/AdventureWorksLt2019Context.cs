72f35f4dcc37f3d4e2b03d0bd05d0080b7ce2e8f
Betacom <Betacom@DESKTOP-LFJB28A>
2025-04-23 17:00:11 +0200

    Remove override adventureworks2019context

--- Models/AdventureWorksLt2019Context.cs
+++ Models/AdventureWorksLt2019Context.cs
@@ -49,10 +49,6 @@ public partial class AdventureWorksLt2019Context : DbContext
 
     public virtual DbSet<VProductModelCatalogDescription> VProductModelCatalogDescriptions { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-SSS64FU\\SQLEXPRESS01; Initial Catalog = AdventureWorksLT2019; Integrated Security = True; Encrypt=True;Trust Server Certificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
