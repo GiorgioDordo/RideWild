2de9d2242459b4e7da0583cb9d97933b4de14f10
GiorgioDordo <djole6789@gmail.com>
2025-05-07 18:15:08 +0200

    Refactor ProductsController and add ProductDTO
    Updated ProductsController to initialize context and replace
    PostProduct with a new implementation using ProductDTO.
    Commented out unused methods and improved exception handling.
    Added ProductDTO class for data validation and transfer.
    Introduced ProductCategory property in AdventureWorksLt2019Context
    for future category management.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using RideWild.DTO;
 using RideWild.Models.AdventureModels;
 
 namespace RideWild.Controllers
@@ -15,17 +16,25 @@ namespace RideWild.Controllers
     {
         private readonly AdventureWorksLt2019Context _context;
 
+
         public ProductsController(AdventureWorksLt2019Context context)
         {
             _context = context;
         }
-
+        
         // GET: api/Products
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
             return await _context.Products.ToListAsync();
         }
+
+        // GET: api/ProductCatergory
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategory()
+        //{
+        //    return await _context.ProductCategory.Select(static c =>c.Name).ToListAsync();
+        //}
 
         // GET: api/Products/5
         [HttpGet("{id}")]
@@ -43,44 +52,71 @@ namespace RideWild.Controllers
 
         // PUT: api/Products/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
-        {
-            if (id != product.ProductId)
-            {
-                return BadRequest();
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutProduct(int id, ProductDTO productDTO)
+        //{
+        //    Product product = new Product
+        //    {
+        //        Name = productDTO.Name,
+        //        ProductNumber = productDTO.ProductNumber,
+        //        Color = productDTO.Color,
+        //        StandardCost = productDTO.StandardCost,
+        //        ListPrice = productDTO.ListPrice,
+        //        Size = productDTO.Size,
+        //        Weight = productDTO.Weight,
+        //        ProductCategoryId = productDTO.ProductCategoryId,
+        //        ProductModelId = productDTO.ProductModelId,
+        //        SellStartDate = productDTO.SellStartDate,
+        //        SellEndDate = productDTO.SellEndDate
+        //    };
+        //    if (id != product.ProductId)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    _context.Products.Update(product);
+        //    await _context.SaveChangesAsync();
 
-            _context.Entry(product).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
+        //    return CreatedAtAction(nameof(PutProduct), new { }, productDTO);
+        //}
 
         // POST: api/Products
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<Product>> PostProduct(ProductDTO productDTO)
         {
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+            Product product = new Product
+            {
+                Name = productDTO.Name,
+                ProductNumber = productDTO.ProductNumber,
+                Color = productDTO.Color,
+                StandardCost = productDTO.StandardCost,
+                ListPrice = productDTO.ListPrice,
+                Size = productDTO.Size,
+                Weight = productDTO.Weight,
+                ProductCategoryId = productDTO.ProductCategoryId,
+                ProductModelId = productDTO.ProductModelId,
+                SellStartDate = productDTO.SellStartDate == default ? DateTime.UtcNow : productDTO.SellStartDate,
+                SellEndDate = null, // Set to null if not provided,
+                ThumbNailPhoto = productDTO.ThumbNailPhoto,
+                ThumbnailPhotoFileName = productDTO.ThumbnailPhotoFileName,
+            };
+
+            try
+            {
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+            }
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ProductId }, product);
         }
 
         // DELETE: api/Products/5
