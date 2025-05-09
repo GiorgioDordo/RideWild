1b6d829305674a54dc911a951a5d4021d0708e28
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2025-05-09 12:44:21 +0200

    CRUD Cart

---
+++ DTO/CartItemDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace RideWild.DTO
+{
+    public class CartItemDTO
+    {
+        public long CartItemId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
